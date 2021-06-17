0a67aae2ca978125193c9324027b46e1f8bf8442
gt-his <80899243+gt-his@users.noreply.github.com>
2021-06-17 23:21:48 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@
   const open = document.getElementById('open');
   const overlay = document.querySelector('.overlay');
   const close = document.getElementById('close');
+  const navi = document.getElementById('navi');
 
   open.addEventListener('click', () => {
     overlay.classList.add('show');
@@ -11,6 +12,11 @@
   });
 
   close.addEventListener('click', () => {
+    overlay.classList.remove('show');
+    open.classList.remove('hide');
+  });
+
+  navi.addEventListener('click', () => {
     overlay.classList.remove('show');
     open.classList.remove('hide');
   });
