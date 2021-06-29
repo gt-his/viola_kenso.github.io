b821d13ffc93e4433efad99c6dae1b072b5aad4e
gt-his <80899243+gt-his@users.noreply.github.com>
2021-06-29 23:43:53 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -22,3 +22,37 @@
   });
 
 }
+
+$(function () {
+  // 上に戻るボタンの初期化
+  let topBtn = $('#back');
+  topBtn.hide();
+
+  // ある程度スクロールされたら、上に戻るボタンを表示する
+  $(window).scroll(function(){
+    if ($(this).scrollTop() > 2000) {
+      topBtn.fadeIn(); // フェードインで表示
+    }else{
+      topBtn.fadeOut();
+    }
+  });
+
+  // クリックで上に戻るボタン
+  topBtn.click(function (event) {
+    event.preventDefault(); // 無効化
+    $('body,html').animate({
+      scrollTop: 0
+    },500);
+  });
+});
+
+$(function () {
+  let topBtn2 = $('#scrollTop,#scrollTop2,#scrollTop3');
+  // topBtn2.hide();
+  topBtn2.click(function (event) {
+    event.preventDefault();
+    $('body,html').animate({
+      scrollTop: 0
+    },500);
+  });
+});
